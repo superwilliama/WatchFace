b760c47694a538cd39014d51cb0e298d7692f3dd
William Andersen <superwilliama@gmail.com>
2020-01-19 12:20:57 +0100

    First commit

---
+++ Face Extension/GameScene.swift
@@ -0,0 +1,35 @@
+import SpriteKit
+class GameScene: SKScene {
+    //Define SKSpriteNode for watch hands
+    var secondHand:SKSpriteNode = SKSpriteNode()
+    var minuteHand:SKSpriteNode = SKSpriteNode()
+    var hourHand:SKSpriteNode = SKSpriteNode()
+    override func sceneDidLoad() {
+        if let secHand:SKSpriteNode = self.childNode(withName: "SecondHand") as? SKSpriteNode{
+            secondHand = secHand
+        }
+        if let minHand:SKSpriteNode = self.childNode(withName: "MinuteHand") as? SKSpriteNode{
+            minuteHand = minHand
+        }
+        if let hrHand:SKSpriteNode = self.childNode(withName: "HourHand") as? SKSpriteNode{
+            hourHand = hrHand
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = CGFloat(calendar.component(.hour, from: date))
+        let minutes = CGFloat(calendar.component(.minute, from: date))
+        let seconds = CGFloat(calendar.component(.second, from: date))
+        secondHand.zRotation = -1 * deg2rad(seconds * 6)
+        minuteHand.zRotation = -1 * deg2rad(minutes * 6)
+        hourHand.zRotation = -1 * deg2rad(hour * 30 + minutes/2)
+    }
+
+    func deg2rad(_ number: CGFloat) -> CGFloat {
+        return number * .pi / 180
+    }
+
+}
